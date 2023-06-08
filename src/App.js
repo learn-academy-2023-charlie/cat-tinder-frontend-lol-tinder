d04c241e8db573786c7d06fb91054d407929faff
bkre12345 <belvira@terpmail.umd.edu>
2023-06-08 14:49:43 -0400

    fixed routing issues

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import ChampIndex from "./pages/ChampIndex";
@@ -8,25 +8,28 @@ import ChampNew from "./pages/ChampNew";
 import ChampEdit from "./pages/ChampEdit";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
-import mockchamps from "./mockchamps";
+import mockChamp from "./MockChamp";
 import "./App.css";
 
 const App = () => {
-  const [champs, setChamps] = useState(mockchamps);
+  const [champions, setChampions] = useState(mockChamp);
 
   return (
-    <>
+    <Router>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/champIndex" element={<ChampIndex champs={champs} />} />
-        <Route path="/champShow/:id" element={<ChampShow champs={champs} />} />
+        <Route path="/champIndex" element={<ChampIndex champs={champions} />} />
+        <Route
+          path="/champShow/:id"
+          element={<ChampShow champs={champions} />}
+        />
         <Route path="/champNew" element={<ChampNew />} />
         <Route path="/champEdit" element={<ChampEdit />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
       <Footer />
-    </>
+    </Router>
   );
 };
 
